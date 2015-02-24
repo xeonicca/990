466615fd0067bf0f9d375e4b089c7fec0c28505c
Zero <john.royer@gmail.com>
2015-02-24 11:43:15 +0800

    Add Auth Controller for RESTful API

---
+++ fuel/app/classes/authrest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * RESTful Controller 專用登入驗證工具
+ */
+class AuthRest extends Controller_Rest {
+  protected $default_format = 'json';
+
+  public function before(){
+    parent::before();
+
+    $this->rest_format = 'json';
+
+    // 檢查是否已經登入
+    if(true){
+      // donothing
+    }else{
+      // 尚未登入
+      return new Response('login needed', 401);
+    }
+  }
+}
