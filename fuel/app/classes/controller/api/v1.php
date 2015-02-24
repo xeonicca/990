0a17e6f42f01aa782875774651208feae4e18b7e
Zero <john.royer@gmail.com>
2015-02-24 16:57:36 +0800

    Add User Infomation Getter

--- fuel/app/classes/controller/api/v1.php
+++ fuel/app/classes/controller/api/v1.php
@@ -9,4 +9,28 @@ class Controller_Api_v1 extends AuthRest {
   public function get_data(){
     $this->response(array(123));
   }
+
+  /**
+   * 讀取使用者資料
+   *
+   * 依照 user ID 回傳使用者資料。
+   * 使用者不存在則回傳 HTTP 404。
+   *
+   * @param int $id user ID
+   */
+  public function get_user($id){
+    $user = Model_User::find($id);
+
+    if($user === null){
+      return $this->response('user not found', 404);
+    }
+
+    return $this->response(array(
+      'id' => $user->id,
+      'name' => $user->name,
+      'nick' => $user->nick,
+      'email' => $user->email,
+      'groupId' => $user->gid,
+    ));
+  }
 }
